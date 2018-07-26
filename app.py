9890855f135b5df42a813f1bb6d19aa6e430c042
vFeng <1914007838@qq.com>
2018-07-26 10:51:24 +0800

    修改app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.debug = True
@@ -6,7 +6,7 @@ app.debug = True
 
 @app.route('/')
 def hello_world():
-    return 'Hello 王木木!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
